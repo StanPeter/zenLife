efa42ad0df51e0ccf407a7de0fd0a0fbeda28b3d
stan05 <stanislav.petr7@gmail.com>
2019-06-08 20:19:34 +0100

    added post_detail.html to see detail view of each post

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
+    # 127.0.0.1:8000
+    # myApp.com 
     path('', views.post_list, name = 'post_list'),
+    
+    # 127.0.0.1:8000/post/1
+    # myApp.com/post/1
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .models import Post
 
 
 # Create your views here.
@@ -8,3 +8,10 @@ def post_list(request):    #templates/blogApp/post_list.html but tempaltes is au
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts': posts}
     return render(request, 'blogApp/post_list.html', stuff_for_frontend)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blogApp/post_detail.html', stuff_for_frontend)
+    
+    
